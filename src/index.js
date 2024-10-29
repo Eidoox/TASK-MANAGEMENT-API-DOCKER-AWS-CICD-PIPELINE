a1fa38d3da4c2755a834dca921b5ac043c75eb27
eidoox <ahmedeidabdullah99@gmail.com>
2024-10-29 05:58:38 +0300

    Finish the setup of nginx

--- src/index.js
+++ src/index.js
@@ -21,6 +21,10 @@ app.use(express.json());
       next();
     });
 
+    app.get("/", (req, res) => {
+      res.send("Welcoe to my nodeappppppppp");
+    });
+
     app.use("/api/v1/", indexRouter);
 
     app.listen(5000, () => {
